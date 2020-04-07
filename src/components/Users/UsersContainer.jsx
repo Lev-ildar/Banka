64090a1c3031b381f2506121463405beff863f35
Lev-ildar <levildar@gmail.com>
2020-04-07 17:23:30 +0500

    Users pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -35,7 +35,7 @@ class UsersContainer extends React.Component {
 
 
     render() {
-        return <>>>
+        return <>
             { this.props.isFetching ? <Preloader/> : null }
             <Users totalUsersCount={this.props.totalUsersCount}
                       pageSize={this.props.pageSize}
