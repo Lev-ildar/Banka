24f24c693f84a4d9fc90be55ed27cba308b07bb4
Lev-ildar <levildar@gmail.com>
2020-04-15 20:31:04 +0500

    componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 
 const ProfileInfo = (props) => {
@@ -10,11 +11,12 @@ const ProfileInfo = (props) => {
 
     return (
     <div>
-      <div>
+      {/*<div>
         <img src='https://avatars.mds.yandex.net/get-pdb/27625/ee3b7ee2-c06f-4c8e-8a6f-f940eb944c3d/s800' alt="F" />
-      </div>
+      </div>*/}
       <div className={s.descriptionBlock}>
           <img src={props.profile.photos.large} />
+          <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
       </div>
 
         <div className={s.descriptionBlock}><h3>{props.profile.aboutMe}</h3></div>
