67e4c62423bb704d3870d57bcdb8488b1259b16a
Lev-ildar <levildar@gmail.com>
2020-05-02 20:24:42 +0500

    LazySuspense

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Settings from "./components/Settings/Settings";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./Login/Login";
 import {connect, Provider} from "react-redux";
@@ -15,6 +13,9 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
+
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
 
 class App extends React.Component {
     componentDidMount() {
@@ -26,10 +27,11 @@ class App extends React.Component {
             return <Preloader/>
         }
         return (
-            <div className='app-wrapper'>
+                <div className='app-wrapper'>
                 <HeaderContainer/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
+                    <React.Suspense fallback={<Preloader/>}>
                     <Route path='/dialogs'
                            render={() => <DialogsContainer/>}/>
                     <Route path='/profile/:userId?'
@@ -41,9 +43,10 @@ class App extends React.Component {
                     <Route path='/music' component={Music}/>
                     <Route path='/news' component={News}/>
                     <Route path='/settings' component={Settings}/>
+                    </React.Suspense>
                 </div>
             </div>
-        );
+                    );
     }
 }
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -11,7 +11,7 @@ const FOLLOWING_IN_PROGRESS = 'FOLLOWING_IN_PROGRESS';
 
 let initialState = {
     users: [ ],
-    pageSize: 5,
+    pageSize: 10,
     totalUsersCount: 0,
     currentPage: 1,
     isFetching: true,
